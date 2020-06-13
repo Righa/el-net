cf3dc8726d9156a0d5deb2512d9efa680593bc14
Righa <54580993+Righa@users.noreply.github.com>
2020-06-13 18:11:38 +0300

    all api test

--- app/Http/Controllers/Api/ForumsController.php
+++ app/Http/Controllers/Api/ForumsController.php
@@ -7,26 +7,95 @@ use Illuminate\Http\Request;
 
 class ForumsController extends Controller
 {
-	public function open()
-	{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
 		return response()->json([
             'success' => true,
             'message' => 'open forum'
         ]);
-	}
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+
+
+
+
+
+
 	public function close()
 	{
 		# all c
 	}
-	public function delete()
-	{
-		# all c
-	}
-	public function update()
-	{
-		# all c
-	}
-
 
 	public function answer()
 	{
